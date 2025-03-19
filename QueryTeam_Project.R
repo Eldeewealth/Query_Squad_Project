9c8517d8eda32550f9dfb2fee99d7f393c4b5999
Loveday <52781967+Eldeewealth@users.noreply.github.com>
2025-03-20 00:00:33 +0100

    Add Query_Squad Project.R first commit

--- QueryTeam_Project.R
+++ QueryTeam_Project.R
@@ -1,4 +1,15 @@
-library(tidyverse)
-library(lubridate)
-library(ggplot2)
+# Install and load required libraries
+install.packages("rvest") # For web scraping
+install.packages("httr") # For handling HTTP requests
+install.packages("dplyr") # For data manipulation
+install.packages("stringr") # For string manipulation
+# Load the libraries
+library(rvest)
+library(httr)
+library(dplyr)
+library(stringr)
+
+# Initialize empty lists to store data for all pages of both websites
+all_pages_data_theaa <- list()
+all_pages_data_cinch <- list()
 
