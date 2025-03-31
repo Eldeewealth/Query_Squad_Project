c195b0fad3cb12e0c3df410cacf72f0504bfb2b8
Loveday <52781967+Eldeewealth@users.noreply.github.com>
2025-03-31 02:23:53 +0100

    added Mice library

--- QueryTeam_Project.R
+++ QueryTeam_Project.R
@@ -10,7 +10,7 @@ library(dplyr) # For data manipulation and transformation
 library(stringr) # For string manipulation and regular expressions
 library(ggplot2) # For data visualization and plotting
 library(ggcorrplot) # For correlation heatmap visualization
-
+library(mice) # For handling missing data and imputation in R
 
 # Initialize empty lists to store data for all pages of both websites
 all_pages_data_theaa <- list()
